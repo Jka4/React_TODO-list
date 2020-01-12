81c0744ce542bd526e8c49bba6db22655e520676
jka4 <plotnik1295@gmail.com>
2020-01-12 16:54:39 +0200

    some fix

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import "./App.css";
 
 function Todo({ todo, index, completeTodo, removeTodo }) {
   return (
-    <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "none" }}
-    >
-      {todo.text}
+    <div className='todo'>
+      <p
+        className='todo_text'
+        style={{ textDecoration: todo.isCompleted ? "line-through" : "none" }}>
+        <div className='todo_buttons'>
+          <svg
+            aria-hidden='true'
+            data-prefix='fas'
+            data-icon='check'
+            class='svg-inline--fa fa-check fa-w-16 check'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 512 512'>
+            {todo.isCompleted && (
+              <path
+                fill='green'
+                d='M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z'
+              />
+            )}
+          </svg>
 
-      <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>x</button>
-      </div>
+          <button onClick={() => completeTodo(index)}>Complete</button>
+          <button onClick={() => removeTodo(index)}>x</button>
+        </div>
+        {todo.text}
+      </p>
     </div>
   );
 }
@@ -30,11 +45,12 @@ function TodoForm({ addTodo }) {
   return (
     <form onSubmit={handleSubmit}>
       <input
-        type="text"
-        className="input"
+        type='text'
+        className='input'
         placeholder='Новая задача'
         value={value}
         onChange={e => setValue(e.target.value)}
+        autoFocus
       />
       <button className='addButton'>Add</button>
     </form>
@@ -42,7 +58,7 @@ function TodoForm({ addTodo }) {
 }
 
 function App() {
-  const [todos, setTodos] = useState([
+  let [todos, setTodos] = useState([
     {
       text: "Сделать тудушку",
       isCompleted: false
@@ -66,8 +82,8 @@ function App() {
     const newTodos = [...todos];
 
     newTodos[index].isCompleted !== true
-      ? newTodos[index].isCompleted = true
-      : newTodos[index].isCompleted = false
+      ? (newTodos[index].isCompleted = true)
+      : (newTodos[index].isCompleted = false);
 
     setTodos(newTodos);
   };
@@ -79,9 +95,8 @@ function App() {
   };
 
   return (
-    <div className="app">
-      <div className="todo-list">
-
+    <div className='app'>
+      <div className='todo-list'>
         <TodoForm addTodo={addTodo} />
 
         {todos.map((todo, index) => (
